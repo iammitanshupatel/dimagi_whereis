41fc5a23f02a8c48b8681847944a6f9bfc236dd9
Mitanshu Patel <mitanshu.u.patel@gmail.com>
2024-03-27 21:24:54 +0530

    commit

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const rateLimit = require("axios-rate-limit");
 
 const geonamesClient = rateLimit(axios.create(), {
-  maxRPS: 10,
+  maxRPS: 5,
   windowMs: 3600000,
 });
 
